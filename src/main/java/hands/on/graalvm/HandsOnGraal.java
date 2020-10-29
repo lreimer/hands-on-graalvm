c7d244b6f4a2320bb0397f1752757d9b9dd13705
Mario-Leander Reimer <Mario-Leander.Reimer@partner.bmw.de>
2020-10-29 01:15:19 +0100

    Added quiet option.

--- src/main/java/hands/on/graalvm/HandsOnGraal.java
+++ src/main/java/hands/on/graalvm/HandsOnGraal.java
@@ -2,15 +2,22 @@ package hands.on.graalvm;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Command(version = "@|yellow Hands on GraalVM 1.0|@", mixinStandardHelpOptions = true)
 class HandsOnGraal implements Runnable {
 
+    private static final Logger LOGGER = Logger.getLogger(HandsOnGraal.class.getName());
+
     @Parameters(arity = "0..1", description = "The message to echo.")
     private String message;
+
+    @Option(names = "--quiet", description = "No ouput.")
+    boolean quiet = false;
 
     public static void main(String[] args) {
         CommandLine.run(new HandsOnGraal(), args);
@@ -18,7 +25,9 @@ class HandsOnGraal implements Runnable {
 
     @Override
     public void run() {
-        String output = Optional.ofNullable(message).orElse("Hands on GraalVM.");
-        System.out.println(output);
+        if (!quiet) {
+            String output = Optional.ofNullable(message).orElse("Hands on GraalVM.");
+            LOGGER.info(output);
+        }
     }
 }
